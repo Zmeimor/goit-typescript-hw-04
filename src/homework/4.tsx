207864e779d82063ef1866603ede8b4dc0cb07dd
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-10-29 19:37:03 +0100

    пп

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -3,26 +3,31 @@ import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
-
+type MenuSelected = { id: MenuIds };
 // Додати тип Menu Selected
 
-const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
-});
+type MenuAction = {
+  onSelectedMenu: (menu: MenuSelected) => void;
+};
 
 // Додайте тип MenuAction
+const MenuSelectedContext = createContext<MenuSelected>({
+  id: "first",
+});
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<MenuSelected>({
+    id: "first",
+  });
 
   const menuContextAction = useMemo(
     () => ({
@@ -30,17 +35,15 @@ function MenuProvider({ children }: PropsProvider) {
     }),
     []
   );
-
   const menuContextSelected = useMemo(
     () => ({
       selectedMenu,
     }),
     [selectedMenu]
   );
-
   return (
     <MenuActionContext.Provider value={menuContextAction}>
-      <MenuSelectedContext.Provider value={menuContextSelected}>
+    <MenuSelectedContext.Provider value={selectedMenu}>
         {children}
       </MenuSelectedContext.Provider>
     </MenuActionContext.Provider>
@@ -48,12 +51,12 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
   const { onSelectedMenu } = useContext(MenuActionContext);
-  const { selectedMenu } = useContext(MenuSelectedContext);
+  const selectedMenu = useContext(MenuSelectedContext);
 
   return (
     <>
